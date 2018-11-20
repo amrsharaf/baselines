c7adf043fb62a3e88742708c98b2b220d40b45ff
amrsharaf <amr.n.sharaf@gmail.com>
2018-11-20 14:37:41 -0500

    single arm at random baseline

--- meta_baselines.py
+++ meta_baselines.py
@@ -4,6 +4,8 @@ import gym_bandit.utils.dp as dp
 import gym_bandit.utils.rl as rl
 from gym.envs.registration import register
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from multiprocessing import cpu_count
 
 
 class Policy(object):
@@ -49,10 +51,22 @@ class SupervisedPolicy(object):
         return action
 
 
+class SinglePolicy(object):
+    def __call__(self, env, observation, costs):
+        # Always single arm meta-action, a single arm selected uniformly at random
+        state = observation_to_state(observation=observation)
+        action = int(dp.MetaAction.single)
+        if not rl.is_allowed_action(state=state, action=action, costs=costs):
+            action = int(dp.MetaAction.none)
+        return action
+
+
 def number_of_arms_pulled(action):
     if action == int(dp.MetaAction.none):
         return 0
     elif action == int(dp.MetaAction.bandit):
+        return 1
+    elif action == int(dp.MetaAction.single):
         return 1
     else:
         assert action == int(dp.MetaAction.supervised)
@@ -117,14 +131,17 @@ def supervised_baseline(n_episodes, env, costs, max_arm_pulls):
                       max_arm_pulls=max_arm_pulls)
 
 
+def single_baseline(n_episodes, env, costs, max_arm_pulls):
+    return run_bandit(n_episodes=n_episodes, env=env, costs=costs, policy=SinglePolicy(), max_arm_pulls=max_arm_pulls)
+
+
 # Creates a muti-arm bandit environment
-def create_bandit_environment(budget, horizon, costs):
+def register_bandit_env(budget, horizon, costs):
     # Create an instance of the bandit environment
     register(id='bandit-v1', entry_point='gym_bandit.learning:BanditEnv', kwargs={'budget': budget,
                                                                                   'horizon': horizon,
                                                                                   'costs': costs})
-    env = gym.make('bandit-v1')
-    return env
+    print('registered!')
 
 
 # TODO print confidence intervals as well
@@ -138,7 +155,8 @@ def all_baselines():
     costs = {}
     costs[dp.MetaAction.bandit] = bandit_cost
     costs[dp.MetaAction.supervised] = supervised_cost
-    env = create_bandit_environment(budget=budget, horizon=horizon, costs=costs)
+    register_bandit_env(budget=budget, horizon=horizon, costs=costs)
+    env = gym.make('bandit-v1')
     bandit_baseline(n_episodes, env, supervised_cost=supervised_cost, bandit_cost=bandit_cost, max_arm_pulls=math.inf)
     supervised_baseline(n_episodes, env, supervised_cost=supervised_cost, bandit_cost=bandit_cost,
                         max_arm_pulls=math.inf)
@@ -146,18 +164,51 @@ def all_baselines():
     random_baseline(n_episodes, env, supervised_cost=supervised_cost, bandit_cost=bandit_cost, max_arm_pulls=math.inf)
 
 
-def main():
-    # TODO abstract away the common parameters with the all_baselines method
+def bandit_accuracy_for_threhsold(threshold):
+    # TODO can we use one env instance instead of creating a new one each time?
+    print('threshold: ', threshold)
     n_episodes = 100
-    budget = 400000
-    horizon = 400
+    max_arm_pulls = threshold
+    costs = get_costs()
+    env = gym.make('bandit-v1')
+    bandit_accuracy = bandit_baseline(n_episodes=n_episodes, env=env, costs=costs, max_arm_pulls=max_arm_pulls)
+    return bandit_accuracy
+
+
+def supervised_accuracy_for_threshold(threshold):
+    print('threshold: ', threshold)
+    n_episodes = 100
+    max_arm_pulls = threshold
+    costs = get_costs()
+    env = gym.make('bandit-v1')
+    supervised_accuracy = supervised_baseline(n_episodes=n_episodes, env=env, costs=costs, max_arm_pulls=max_arm_pulls)
+    return supervised_accuracy
+
+
+def single_accuracy_for_threshold(threshold):
+    print('threshold: ', threshold)
+    n_episodes = 100
+    max_arm_pulls = threshold
+    costs = get_costs()
+    env = gym.make('bandit-v1')
+    single_accuracy = single_baseline(n_episodes=n_episodes, env=env, costs=costs, max_arm_pulls=max_arm_pulls)
+    return single_accuracy
+
+
+def get_costs():
     supervised_cost = 2
     bandit_cost = 1
+    single_cost = 1
     costs = {}
     costs[dp.MetaAction.bandit] = bandit_cost
     costs[dp.MetaAction.supervised] = supervised_cost
+    costs[dp.MetaAction.single] = single_cost
+    return costs
+
+
+def main():
+    # TODO abstract away the common parameters with the all_baselines method
     # Create an instance of the bandit environment
-    env = create_bandit_environment(budget=budget, horizon=horizon, costs=costs)
     # x-axis: number of arm pulls
     # TODO what is the maximum number of arms to plot?
     max_arm_pulls = 200
@@ -166,14 +217,19 @@ def main():
     # TODO this is so slow
     step = 10
     thresholds = []
-    for threshold in range(0, max_arm_pulls, step):
-        thresholds.append(threshold)
-        print('threshold: ', threshold)
-        max_arm_pulls = threshold
-        bandit_accuracy.append(bandit_baseline(n_episodes=n_episodes, env=env, costs=costs,
-                                               max_arm_pulls=max_arm_pulls))
-        supervised_accuracy.append(supervised_baseline(n_episodes=n_episodes, env=env, costs=costs,
-                                                       max_arm_pulls=max_arm_pulls))
+    # Run every threshold in parallel
+    n_cpus = cpu_count()
+    pool = Pool(n_cpus)
+    print('pool: ', pool)
+    print('number of cpus: ', n_cpus)
+    thresholds = range(0, max_arm_pulls, step)
+    budget = 400000
+    horizon = 400
+    costs = get_costs()
+    register_bandit_env(budget=budget, horizon=horizon, costs=costs)
+    single_accuracy = list(map(single_accuracy_for_threshold, thresholds))
+    bandit_accuracy = list(map(bandit_accuracy_for_threhsold, thresholds))
+    supervised_accuracy = list(map(supervised_accuracy_for_threshold, thresholds))
     # Now we need to get the best arm identification accuracy for the y_axis for both bandit and supervised baselines
     # I think we need to update the run function to take extra input the maximum allowed arm pulls
     print('thresholds: ', thresholds)
@@ -181,6 +237,7 @@ def main():
     print('supervised_accuracy: ', supervised_accuracy)
     plt.plot(thresholds, bandit_accuracy, label='bandit')
     plt.plot(thresholds, supervised_accuracy, label='supervised')
+    plt.plot(thresholds, single_accuracy, label='single')
     plt.legend()
     plt.show()
 
